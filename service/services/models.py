849bc3802067acd171ac8c7aa2a5c679a5d55d43
sergey <grigoryev0089@gmail.com>
2023-07-10 19:48:01 +0300

    Create models

--- service/services/models.py
+++ service/services/models.py
@@ -20,7 +20,7 @@ class Plan(models.Model):
     discount_percent = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
 
 
-class Subsscription(models.Model):
+class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
