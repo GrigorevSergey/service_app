c5f84f8d5eb7ff4cf1d82718c3881c72750c2c79
sergey <grigoryev0089@gmail.com>
2023-07-13 22:43:55 +0300

    Create serializer+urls

--- service/services/views.py
+++ service/services/views.py
@@ -8,3 +8,4 @@ class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related('client').prefetch_related('client__user')
     serializer_class = SubscriptionSerializer
 
+
